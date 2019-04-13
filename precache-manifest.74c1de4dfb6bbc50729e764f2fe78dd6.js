d5cc5c39eddb66e5801ae91436ea31b23e9eb3f6
Kate <kef12@comcast.net>
2019-04-13 15:28:27 -0600

    Updates

---
+++ precache-manifest.74c1de4dfb6bbc50729e764f2fe78dd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/google-book-search/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d536b4b13827ad40f1bc",
+    "url": "/google-book-search/static/js/runtime~main.aaf871bd.js"
+  },
+  {
+    "revision": "a4bfc3a3b30f9e887cbd",
+    "url": "/google-book-search/static/js/main.ae310a46.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/google-book-search/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "a4bfc3a3b30f9e887cbd",
+    "url": "/google-book-search/static/css/main.8a069751.chunk.css"
+  },
+  {
+    "revision": "87cc293bb84db290d52360649c52de90",
+    "url": "/google-book-search/index.html"
+  }
+];
